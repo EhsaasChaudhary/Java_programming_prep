bd41ab8449d7352fe0b368cdd601d90f235c525c
EhsaasChaudhary <116794955+EhsaasChaudhary@users.noreply.github.com>
2024-06-11 02:33:57 +0530

    Create missingnum.java

---
+++ missingnum.java
@@ -0,0 +1,21 @@
+/**
+ * missingnum
+ */
+public class missingnum {
+
+    static int getMissingNo(int a[], int n)
+    {
+        int total = 1;
+        for (int i = 2; i < (n + 1); i++) {
+            total += i;
+            total -= a[i - 2];
+        }
+        return total;
+    }
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 5 };
+        int N = arr.length;
+      
+        System.out.println(getMissingNo(arr, N));
+    }
+}
